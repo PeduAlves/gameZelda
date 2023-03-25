966e292a39715f85de6033a6bd587e88f4444411
Pedro Alves <pedrosilveira@cc.ci.ufpb.br>
2023-03-24 21:46:58 -0300

    Adicionando rotação do Player e algumas animações
    Adicionando a rotação do personagem para onde voce anda.
    adicionando animações:
    Correr
    Idle
    Ataque de espada

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Animator animator;
     private CharacterController controller;
+    private bool isWalk;
     private Vector3 direction;
 
     //Header serve para aparecer o titulo como caixinha na unity
@@ -14,14 +16,33 @@ public class PlayerController : MonoBehaviour
     void Start(){
 
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
     void Update(){
         
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
+        if( Input.GetButtonDown("Fire1") ){
+
+            animator.SetTrigger("Attack");
+        }
+
         direction = new Vector3( horizontal, 0, vertical ).normalized;
 
+        if( direction.magnitude > 0.1f){
+            
+            float targetAngle = Mathf.Atan2( direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler( 0, targetAngle, 0);
+            isWalk = true;
+        }
+        else{
+
+            isWalk = false;
+        }
+
         controller.Move( direction * MovementSpeed * Time.deltaTime );
+
+        animator.SetBool("isWalk", isWalk);
     }
 }
